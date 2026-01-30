a8e77cc48469a964cfd9ca0b1011ffb32e5ac5f1
riyazuddin-1 <riyazuddin9846@gmail.com>
2026-01-30 23:44:39 +0530

    feat: Implement auth manager with login, signup handlers, and validation middleware

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRoutes from "./modules/auth/auth.routes.js";
+import authRoutes from "./modules/auth-manager/auth.routes.js";
 import urlRoutes from "./modules/url/url.routes.js";
 import qrRoutes from "./modules/qr/qr.routes.js";
 import { PORT } from "./shared/config/env.js";
@@ -16,3 +16,9 @@ app.use("/u", urlRoutes); // for urls
 app.use("/qr", qrRoutes); // for qr
 
 app.listen(PORT, () => console.log(`running on ${PORT}`));
+
+/**
+ * Auth manager
+ * Password manager
+ * Network tunnels
+ */
